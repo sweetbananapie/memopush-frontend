4433e9148b651c29dd93248a0ad73cb4f209e96e
Danila Yermolchev <ermolchevddd@gmail.com>
2026-04-24 06:34:23 +0500

    amend

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import App from "./App.vue";
 import router from "./plugins/router";
 import i18n from "./plugins/i18n";
 import setupPinia from "./plugins/pinia";
+import { vMaska } from "maska/vue";
 
 const app = createApp(App);
 
-setupPinia(app);
 app.use(router);
 app.use(i18n);
-
+setupPinia(app);
+app.directive("maska", vMaska);
 app.mount("#app");
